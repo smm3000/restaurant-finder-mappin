9269d94ee88f616467d738d6cfa58eabc77cabb6
samir <s-malik@live.co.uk>
2023-12-13 12:17:43 -0500

    Updated to include Marker

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,30 @@
 import * as React from 'react';
-import Map from 'react-map-gl';
+import ReactMapGL, {Marker} from 'react-map-gl';
+import { useState } from 'react';
+import PlaceIcon from '@mui/icons-material/Place';
 
 function App() {
+  const [viewPort, setViewport] = useState({
+    latitude: 51.5007,
+    longitude: -0.1246,
+    zoom: 12,
+  });
+
   return (
-    <Map
-      mapboxAccessToken={process.env.REACT_APP_MAPBOX}
-      initialViewState={{
-        longitude: -0.1276,
-        latitude: 51.5071,
-        zoom: 12
-      }}
-      style={{width: 600, height: 400}}
-      mapStyle="mapbox://styles/mapbox/streets-v9"
-    />
+    <div className="App">
+      <ReactMapGL
+      {...viewPort}
+        mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+        style={{width: 1200, height: 1200}}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        onViewportChange={(viewPort) => setViewport(viewPort)}
+      >
+        <Marker longitude={-0.1246} latitude={51.5007} offsetLeft={-20} offsetTop={-10}>
+          <PlaceIcon style = {{fontSize: viewPort.zoom * 7, color:"slateblue"}}/>
+      
+        </Marker>
+      </ReactMapGL>
+    </div>
   );
 }
 
