d87803f2b569176d5f2cb97e6137ff3e57adde82
samir <s-malik@live.co.uk>
2023-12-15 11:48:31 -0500

    Updated pop-up card with additional details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,9 @@ import * as React from 'react';
 import ReactMapGL, {Marker, Popup} from 'react-map-gl';
 import { useState } from 'react';
 import PlaceIcon from '@mui/icons-material/Place';
+import StarIcon from '@mui/icons-material/Star';
+import "./app.css"
+
 
 function App() {
   const [viewPort, setViewport] = useState({
@@ -11,18 +14,41 @@ function App() {
   });
 
   return (
-    <div className="App" style={{ width: '70vw', height: '70vh' }}>
+    <div className="App" style={{ width: '100vw', height: '100vh' }}>
       <ReactMapGL
         {...viewPort}
         mapboxAccessToken={process.env.REACT_APP_MAPBOX}
-        width="100%"
-        height="100%"
         mapStyle="mapbox://styles/mapbox/streets-v9"
         onViewportChange={(viewPort) => setViewport(viewPort)}
       >
-       <Marker longitude={-0.1246} latitude={51.5007}>
+        <Marker longitude={-0.1246} latitude={51.5007}>
         <PlaceIcon style={{ color: 'slateblue' }} />
-      </Marker>
+        </Marker>
+        <Popup
+          longitude={-0.1246} 
+          latitude={51.5007}
+          closeButton={true}
+          closeOnClick={false}
+          anchor="left" >
+            <div className="card">
+              <label>Place</label>
+              <h4 className="place">Big Ben</h4>
+              <label>Review</label>
+              <p className="desc"> Nice place </p>
+              <label>Rating</label>
+              <div className="stars">
+              <StarIcon className="star"/>
+              <StarIcon className="star"/>
+              <StarIcon className="star"/>
+              <StarIcon className="star"/>
+              <StarIcon className="star"/>
+              </div>
+              <label>Information</label>
+              <span className="username"> Created by <b>samir</b></span>
+              <span className="date"> 1 hour ago <b></b></span>
+            </div>
+          </Popup>
+
       </ReactMapGL>
     </div>
   );
